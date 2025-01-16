59676d8dea023d318f99e8ffb5d84fcb48802132
Manikanta0702 <126312450+Manikanta0702@users.noreply.github.com>
2025-01-16 16:08:58 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,13 +2,10 @@ import os
 from flask import Flask, request, jsonify
 import openai
 from flask_cors import CORS
-from dotenv import load_dotenv
-
 
 app = Flask(__name__)
 CORS(app)
 
-load_dotenv()
 
 openai.api_key = os.getenv('API_KEY')
 
