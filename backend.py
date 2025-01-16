46e545abf218f93a649b3d435d6229241f732e38
Manikanta0702 <126312450+Manikanta0702@users.noreply.github.com>
2025-01-16 16:39:05 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask, request, jsonify
 import openai
 from flask_cors import CORS
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 CORS(app)
 
+load_dotenv()
 
 openai.api_key = os.getenv('API_KEY')
 
@@ -22,12 +25,12 @@ def transcribe_audio(audio_file_path):
     except Exception as e:
         return f"Error: {str(e)}"
 
-def generate_reply(transcription):
+def generate_reply(transcription, context):
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant that provides human-like responses. And give the response in marked down format."},
+                {"role": "system", "content": f"You are a helpful assistant that provides human-like responses. And here is the context. {context}\nAnd give the response in marked down format."},
                 {"role": "user", "content": transcription},
             ],
             temperature=0.7,
@@ -44,7 +47,7 @@ def process_audio():
             return jsonify({"error": "No file part in the request"}), 400
 
         file = request.files["file"]
-
+        context = request.get("context")
         if file.filename == "":
             return jsonify({"error": "No selected file"}), 400
 
@@ -57,7 +60,7 @@ def process_audio():
             os.remove(audio_file_path)
             return jsonify({"error": transcription}), 400
 
-        response = generate_reply(transcription)
+        response = generate_reply(transcription, context)
 
         os.remove(audio_file_path)  
         return jsonify({"response": response, "trancsript": transcription})
@@ -68,3 +71,4 @@ def process_audio():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
